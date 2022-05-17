3bb11de0bbac517dd12de012eaf5f100a1038d2e
Introvert696 <introvert696@yandex.ru>
2022-05-17 15:00:13 +0400

    updated
    Добавил ссылочки и в js добавил переменную для удобности

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 'use strict';
+let hostName = "http://localhost";
+
 //получаем теги для дальнейшей вставки
 let itemItems = document.querySelector(".items");
 let purchasesDiv = document.querySelector(".purchases");
@@ -6,8 +8,16 @@ let purchasesDiv = document.querySelector(".purchases");
 //массив со всеми предметами
 let itemsArr;
 
+try {
+  let formAction = document.querySelector("form");
+  formAction.action = hostName+"/order/create";
+} catch (error) {
+  //console.log(error);
+}
+
+
 //отправляем запрос на сервак, чтобы получить items
-fetch('http://vh522015.eurodir.ru/items')
+fetch(hostName+'/items')
   .then((response) => {
     return response.json();
   })
@@ -16,14 +26,14 @@ fetch('http://vh522015.eurodir.ru/items')
       //пытаемся создать список для выбора
       createOptionList(data);
     } catch (error) {
-      console.log(error);
+      //console.log(error);
     }
     try {
       //Выводим предметы на главную страницу
       viewAllItems(data);
     } catch (error) {
       //обработка ошибок
-      console.log(error)
+      //console.log(error)
     }
 
   });
@@ -34,10 +44,10 @@ try {
   //обработка нажатий по листу
   optionList.addEventListener("click", (e) => {
     let selectedItemId = e.target.value;
-    console.log(itemsArr);
+    //console.log(itemsArr);
     itemsArr.forEach(el => {
       if (selectedItemId == el.Item_Id) {
-        console.log(el);
+        //console.log(el);
         priceValue.value = el.Item_Price;
       }
     });
@@ -56,7 +66,7 @@ function viewAllItems(data) {
 
     let itemDiv = document.createElement("div");
 
-    itemDiv.innerHTML = `<div class='item shadow'><img src=${el['Item_Pict']} class='ctrobj'><div class='item-text'><p><span>${el['Item_Name']} </span></p><br><p>${el['Item_Desk']} </p></div><div class='price-text'><p>Цена: <span>${el['Item_Price']}  руб</span> </p></div></div>`;
+    itemDiv.innerHTML = `<div class='item shadow'><img src=${el['Item_Pict']} alt="item_pict" class='ctrobj'><div class='item-text'><p><span>${el['Item_Name']} </span></p><br><p>${el['Item_Desk']} </p></div><div class='price-text'><p>Цена: <span>${el['Item_Price']}  руб</span> </p></div></div>`;
     itemItems.appendChild(itemDiv);
 
   });
